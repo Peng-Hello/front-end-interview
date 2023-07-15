4446cefe321a08cc41091f94f1d9cfae155ffe97
Peng <70511773+Peng-Hello@users.noreply.github.com>
2023-07-15 21:16:31 +0800

    feat: 数组 push 方法

---
+++ src/js/Array operation/index.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+
+const roster = ["小明", "小红"];
+
+describe("操作 JS 数组方法", () => {
+  it("push 方法", () => {
+    roster.push("小黄");
+    expect(roster).toStrictEqual(["小明", "小红", "小黄"]);
+  });
+});
