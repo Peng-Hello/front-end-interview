fdfa8998acd1a2bb0e1daa9fa3ef1f902c7d038b
Peng <70511773+Peng-Hello@users.noreply.github.com>
2023-07-15 21:42:35 +0800

    feat: 数组 pop 方法

--- src/js/Array operation/index.spec.ts
+++ src/js/Array operation/index.spec.ts
@@ -7,4 +7,9 @@ describe("操作 JS 数组方法", () => {
     roster.push("小黄");
     expect(roster).toStrictEqual(["小明", "小红", "小黄"]);
   });
+
+  it("pop 方法", () => {
+    roster.pop();
+    expect(roster).toStrictEqual(["小明", "小红"]);
+  });
 });
