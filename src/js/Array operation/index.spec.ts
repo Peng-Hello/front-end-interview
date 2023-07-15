7f71f6cd97bdf93f5924e46b36ac65d74e7a74fb
Peng <70511773+Peng-Hello@users.noreply.github.com>
2023-07-15 21:44:30 +0800

    optimize:优化方法分类

--- src/js/Array operation/index.spec.ts
+++ src/js/Array operation/index.spec.ts
@@ -3,44 +3,46 @@ import { describe, it, expect } from "vitest";
 const roster = ["小明", "小红"];
 
 describe("操作 JS 数组方法", () => {
-  it("push 方法", () => {
-    roster.push("小黄");
-    expect(roster).toStrictEqual(["小明", "小红", "小黄"]);
-  });
-
-  it("pop 方法", () => {
-    roster.pop();
-    expect(roster).toStrictEqual(["小明", "小红"]);
-  });
-
-  describe("sort 方法", () => {
-    const alphabetTable = ["c", "d", undefined, "e", "a", "b"];
-    it("没有传参", () => {
-      const temp = [...alphabetTable]; // 因为 Sort 方法是原地排序的，所以我们拷贝一份出来，以免影响后面测试。
-      // 默认排序是将元素转换为字符串，然后按照它们的 UTF-16 码元值升序排序。
-      expect(temp.sort()).toStrictEqual(["a", "b", "c", "d", "e", undefined]);
+  describe("会修改原数组", () => {
+    it("push 方法", () => {
+      roster.push("小黄");
+      expect(roster).toStrictEqual(["小明", "小红", "小黄"]);
     });
-    it("传了排序函数", () => {
-      // 所有的 undefined 元素都会被排序到数组的末尾，并且不调用 compareFn
-      // a , b 不会是 undefined
-      function compareFn(a: any, b: any) {
-        if (a > b) {
-          return 1; // a > b ，应该返回 >0 , a 往后走
+
+    it("pop 方法", () => {
+      roster.pop();
+      expect(roster).toStrictEqual(["小明", "小红"]);
+    });
+
+    describe("sort 方法", () => {
+      const alphabetTable = ["c", "d", undefined, "e", "a", "b"];
+      it("没有传参", () => {
+        const temp = [...alphabetTable]; // 因为 Sort 方法是原地排序的，所以我们拷贝一份出来，以免影响后面测试。
+        // 默认排序是将元素转换为字符串，然后按照它们的 UTF-16 码元值升序排序。
+        expect(temp.sort()).toStrictEqual(["a", "b", "c", "d", "e", undefined]);
+      });
+      it("传了排序函数", () => {
+        // 所有的 undefined 元素都会被排序到数组的末尾，并且不调用 compareFn
+        // a , b 不会是 undefined
+        function compareFn(a: any, b: any) {
+          if (a > b) {
+            return 1; // a > b ，应该返回 >0 , a 往后走
+          }
+          if (a < b) {
+            return -1; // 负数往前走
+          }
+          // a=b , 保持 ab 位置
+          return 0;
         }
-        if (a < b) {
-          return -1; // 负数往前走
-        }
-        // a=b , 保持 ab 位置
-        return 0;
-      }
-      expect(alphabetTable.sort(compareFn)).toStrictEqual([
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        undefined,
-      ]);
+        expect(alphabetTable.sort(compareFn)).toStrictEqual([
+          "a",
+          "b",
+          "c",
+          "d",
+          "e",
+          undefined,
+        ]);
+      });
     });
   });
 });
